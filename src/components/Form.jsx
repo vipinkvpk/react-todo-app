25ca1af2610858f0205b1f00908571e58a5d36b7
VIPIN K <vipinkvpk@gmail.com>
2026-01-29 21:55:04 +0530

    feat: add toggle complete functionality for todos

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,16 @@
 import {useState} from "react"
 import styles from "./form.module.css"
 export default function Form({todos, setTodos}){
-    const [todo, setTodo] = useState("");
+    // const [todo, setTodo] = useState({"");
+
+    const [todo, setTodo] = useState({name:"", done:false});
+
     
 
     function handleSubmit(e){
         e.preventDefault();
         setTodos([...todos, todo]);
-        setTodo("");
+        setTodo({name:"", done:false});
     
     }
     return (
@@ -16,8 +19,8 @@ export default function Form({todos, setTodos}){
             <div className= {styles.inputContainer}>
                 <input
                     className= {styles.modernInput}
-                    onChange= {(e)=>setTodo(e.target.value)} 
-                    value = {todo}
+                    onChange= {(e)=>setTodo({name:e.target.value, done:false})} 
+                    value = {todo.name}
                     type = "text" 
                     placeholder="Enter todo item..."
                 />
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -6,10 +6,20 @@ export default function TodoItem({item, todos, setTodos}){
         setTodos(todos.filter((todo)=>todo!==item ))
         
     }   
-    return (
+    function handleClick(name){
+        const newArray = todos.map((todo)=>todo.name === name ? {...todo, done : !todo.done} : todo)
+   
+    setTodos(newArray)
+
+ }
+const className = item.done ? styles.completed :  "" 
+return (
     <div className = {styles.item}>
         <div className = {styles.itemName}>
-            {item}
+            <span className= {className} onClick={() =>handleClick(item.name)}>
+                {item.name}
+            </span>
+            
             <span>
                 <button onClick = {()=>handleDelete(item)}className = {styles.deleteButton}>x</button>
             </span>
