c03b825688d02b50e621e9de0d395f108d59cea3
VIPIN K <vipinkvpk@gmail.com>
2026-01-29 23:43:04 +0530

    feat: add sorting for todo list items

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,17 @@
 import TodoItem from "./Todoitem";
 import styles from "./todolist.module.css"
 export default function TodoList({todos, setTodos}){
+    const sortedTodos = todos.slice().sort((a,b)=>Number(a.done)-Number(b.done))
     return (
-    <div className = {styles.list}>
-        {todos.map(item=>(
-                <TodoItem key={item.name} item={item} todos= {todos} setTodos = {setTodos}/>
-            ))}
-    </div>
+        <div className = {styles.list}>
+            {sortedTodos.map(item=>(
+                    <TodoItem 
+                    key={item.name}
+                    item={item}
+                    todos= {todos} 
+                    setTodos = {setTodos}
+                    />
+                ))}
+        </div>
     );
 }
